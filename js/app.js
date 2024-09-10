058925cd170fbf92105ddf10eaabaedf7dc05878
Carlos Figueroa <dannyfigueroav14@gmail.com>
2024-09-10 18:44:47 -0500

    "Updated index.html, removed js/app.js and src/style.css files"

--- js/app.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    mode: 'jit',
-    purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-    theme: {
-      extend: {},
-    },
-    variants: {},
-    plugins: [],
-  }
